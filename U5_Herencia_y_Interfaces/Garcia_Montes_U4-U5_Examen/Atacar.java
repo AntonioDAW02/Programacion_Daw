c7549003d7802b8844063ed8c93d8331c170a879
AntonioDaw1 <agarciam@iesalixar.org>
2020-02-12 12:49:15 +0100

    U5_Examen

--- U5_Herencia_y_Interfaces/Garcia_Montes_U4-U5_Examen/Atacar.java
+++
@@ -1,5 +0,0 @@
-package com.company;
-
-public interface Atacar {
-    String atacarPersonaje(Personaje[] personaje);
-}
--- U5_Herencia_y_Interfaces/Garcia_Montes_U4-U5_Examen/Magia.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public interface Magia {
-    boolean encantar(Personaje[] personaje);
-
-    boolean desencantar(Personaje[] personaje);
-}
